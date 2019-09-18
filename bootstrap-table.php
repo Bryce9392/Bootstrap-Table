5b193207f24ac94b6d8be63f6c9fdad565090b67
Bryce <Bryce.nelson@ucf.edu>
2019-09-18 10:01:14 -0400

    change data table script

--- bootstrap-table.php
+++ bootstrap-table.php
@@ -13,13 +13,13 @@
  * enqueue style and scripts
  */
 function bootstrap_table_scripts() {
-    wp_enqueue_style( 'bootstrap-table-css', 'https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.css' );
-	wp_enqueue_script( 'bootstrap-table-js','https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.js', array( 'jquery' ),'',true );
-	wp_enqueue_script( 'bootstrap-table-localo-js','https://unpkg.com/bootstrap-table@1.15.4/dist/locale/bootstrap-table-zh-CN.min.js', array( 'jquery' ),'',true );
+   // wp_enqueue_style( 'bootstrap-table-css', 'https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.css' );
+//	wp_enqueue_script( 'bootstrap-table-js','https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.js', array( 'jquery' ),'',true );
+//	wp_enqueue_script( 'bootstrap-table-localo-js','https://unpkg.com/bootstrap-table@1.15.4/dist/locale/bootstrap-table-zh-CN.min.js', array( 'jquery' ),'',true );
 	
 	
-	wp_enqueue_style( 'data-tables-css', 'https://cdn.datatables.net/v/bs4/dt-1.10.18/b-1.5.6/b-print-1.5.6/fh-3.1.4/kt-2.5.0/r-2.2.2/sc-2.0.0/datatables.min.css' );
-	wp_enqueue_script( 'data-table-js','https://cdn.datatables.net/v/bs4/dt-1.10.18/b-1.5.6/b-print-1.5.6/fh-3.1.4/kt-2.5.0/r-2.2.2/sc-2.0.0/datatables.min.js', array( 'jquery' ),'',true );
+	wp_enqueue_style( 'data-tables-css', '//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' );
+	wp_enqueue_script( 'data-table-js','//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js', array( 'jquery' ),'',true );
 	
 	wp_enqueue_script( 'data-table', plugin_dir_url( __FILE__ ) . 'js/data-table.js' );
 	
